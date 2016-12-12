5841dfc549898d60b7946f44456e93698b8f0785
Ivan Shynkarenka <chronoxor@gmail.com>
2016-12-12 22:14:50 +0300

    Bugfixing

--- modules/asio.cmake
+++ modules/asio.cmake
@@ -11,7 +11,7 @@ if(NOT TARGET asio)
   endif()
   add_library(asio ${SOURCE_FILES})
   target_compile_definitions(asio PRIVATE ASIO_STANDALONE=1 ASIO_SEPARATE_COMPILATION=1)
-  target_include_directories(asio PRIVATE "asio/asio/include" "openssl/include")
+  target_include_directories(asio PRIVATE "asio/asio/include" ${OPENSSL_INCLUDE_DIR})
   target_link_libraries(asio)
 
   # Module folder
