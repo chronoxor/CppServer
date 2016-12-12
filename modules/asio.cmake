d0881da1a8caf7950fd36fbd85932158cb9b10a7
Ivan Shynkarenka <chronoxor@gmail.com>
2016-12-12 20:06:44 +0300

    Add subprojects

---
+++ modules/asio.cmake
@@ -0,0 +1,20 @@
+if(NOT TARGET asio)
+
+  # Module library
+  file(GLOB SOURCE_FILES "asio/asio/src/*.cpp")
+  if(NOT MSVC)
+    set_source_files_properties(${SOURCE_FILES} PROPERTIES COMPILE_FLAGS "${PEDANTIC_COMPILE_FLAGS}")
+  else()
+    # C4127: conditional expression is constant
+    # C4702: unreachable code
+    set_source_files_properties(${SOURCE_FILES} PROPERTIES COMPILE_FLAGS "${PEDANTIC_COMPILE_FLAGS} /wd4127 /wd4702")
+  endif()
+  add_library(asio ${SOURCE_FILES})
+  target_compile_definitions(asio PRIVATE ASIO_STANDALONE=1 ASIO_SEPARATE_COMPILATION=1)
+  target_include_directories(asio PRIVATE "asio/asio/include" "openssl/include")
+  target_link_libraries(asio)
+
+  # Module folder
+  set_target_properties(asio PROPERTIES FOLDER modules/asio)
+
+endif()
---
+++ modules/nanomsg.cmake
@@ -0,0 +1,43 @@
+if(NOT TARGET nanomsg)
+
+  # Save old compiler flags
+  set(CMAKE_C_FLAGS_OLD ${CMAKE_C_FLAGS})
+
+  # Setup new compiler flags
+  if(NOT MSVC)
+  else()
+    # Disable some compile warnings
+    # C4100: 'identifier' : unreferenced formal parameter
+    # C4152: non standard extension, function/data ptr conversion in expression
+    # C4201: nonstandard extension used : nameless struct/union
+    # C4206: nonstandard extension used : translation unit is empty
+    # C4244: 'conversion' conversion from 'type1' to 'type2', possible loss of data
+    # C4245: 'conversion' : conversion from 'type1' to 'type2', signed/unsigned mismatch
+    # C4267: 'var' : conversion from 'size_t' to 'type', possible loss of data
+    # C4295: 'array' : array is too small to include a terminating null character
+    # C4459: declaration of 'identifier' hides global declaration
+    # C4701: Potentially uninitialized local variable 'name' used
+    # C4702: unreachable code
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} /wd4100 /wd4152 /wd4201 /wd4206 /wd4244 /wd4245 /wd4267 /wd4295 /wd4459 /wd4701 /wd4702")
+  endif()
+
+  # Module flags
+  set(NN_HAVE_CONDVAR 1)
+
+  # Module options
+  SET(NN_STATIC_LIB ON CACHE BOOL "Build static library instead of shared library")
+  SET(NN_ENABLE_DOC OFF CACHE BOOL "Disable building documentation")
+  SET(NN_TESTS OFF CACHE BOOL "Disable building nanomsg tests")
+
+  # Module subdirectory
+  add_subdirectory("nanomsg")
+
+  # Module folder
+  set_target_properties(nanomsg PROPERTIES FOLDER modules/nanomsg)
+  set_target_properties(nanocat PROPERTIES FOLDER modules/nanomsg)
+  set_target_properties(dist PROPERTIES FOLDER modules/nanomsg)
+
+  # Restore old compiler flags
+  set(CMAKE_C_FLAGS ${CMAKE_C_FLAGS_OLD})
+
+endif()
