0267eb6649c67e1c310fe77e368d2d3f308850d4
Ivan Shynkarenka <chronoxor@gmail.com>
2016-12-15 05:40:06 +0300

    build

--- modules/asio.cmake
+++ modules/asio.cmake
@@ -3,7 +3,7 @@ if(NOT TARGET asio)
   # Module library
   add_library(asio "asio/asio/src/asio.cpp")
   target_compile_definitions(asio PRIVATE ASIO_STANDALONE=1 ASIO_SEPARATE_COMPILATION=1)
-  target_include_directories(asio PRIVATE "asio/asio/include" ${OPENSSL_INCLUDE_DIR})
+  target_include_directories(asio PRIVATE "asio/asio/include")
   target_link_libraries(asio)
 
   # Module folder
