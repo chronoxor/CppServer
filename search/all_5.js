563f465addc6048f3ecc61f9e404c0a1c66f210e
Travis CI <travis@travis-ci.org>
2016-12-27 01:19:01 +0000

    Update generated documentation

--- search/all_5.js
+++ search/all_5.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['multicast',['Multicast',['../class_cpp_server_1_1_asio_1_1_t_c_p_server.html#a8add1d48e1717d353c67d373d0ded2e0',1,'CppServer::Asio::TCPServer::Multicast()'],['../class_cpp_server_1_1_asio_1_1_u_d_p_server.html#a74916c8898e264923f5c8c2e0f45c48f',1,'CppServer::Asio::UDPServer::Multicast()']]],
-  ['multicast_5fendpoint',['multicast_endpoint',['../class_cpp_server_1_1_asio_1_1_u_d_p_server.html#a311703dd8adc8c8bf7cdc81c9149700d',1,'CppServer::Asio::UDPServer']]]
+  ['joinmulticastgroup',['JoinMulticastGroup',['../class_cpp_server_1_1_asio_1_1_u_d_p_client.html#a73460f8b1f6bcc2c75ddd86c56393699',1,'CppServer::Asio::UDPClient']]]
 ];
