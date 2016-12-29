d61e7ba71aa37015e58061543e7e89df5afd1d1b
Ivan Shynkarenka <chronoxor@gmail.com>
2016-12-29 04:24:03 +0300

    Find OpenSSL library

--- modules/asio.cmake
+++ modules/asio.cmake
@@ -1,9 +1,10 @@
 if(NOT TARGET asio)
 
   # Module library
-  add_library(asio "asio/asio/src/asio.cpp")
+  file(GLOB SOURCE_FILES "asio/asio/src/*.cpp")
+  add_library(asio ${SOURCE_FILES})
   target_compile_definitions(asio PRIVATE ASIO_STANDALONE ASIO_SEPARATE_COMPILATION)
-  target_include_directories(asio PRIVATE "asio/asio/include")
+  target_include_directories(asio PRIVATE "asio/asio/include" ${OPENSSL_INCLUDE_DIR})
   target_link_libraries(asio)
 
   # Module folder
