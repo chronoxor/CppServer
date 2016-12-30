ebb5fd7f0956b89e7c8a5aa1dfdfcd16defe6378
Ivan Shynkarenka <chronoxor@gmail.com>
2016-12-30 19:23:39 +0300

    update

--- modules/asio.cmake
+++ modules/asio.cmake
@@ -1,11 +1,15 @@
 if(NOT TARGET asio)
 
+  # External packages
+  set(OPENSSL_USE_STATIC_LIBS TRUE)
+  find_package(OpenSSL REQUIRED)
+
   # Module library
   file(GLOB SOURCE_FILES "asio/asio/src/*.cpp")
   add_library(asio ${SOURCE_FILES})
   target_compile_definitions(asio PRIVATE ASIO_STANDALONE ASIO_SEPARATE_COMPILATION)
   target_include_directories(asio PRIVATE "asio/asio/include" ${OPENSSL_INCLUDE_DIR})
-  target_link_libraries(asio)
+  target_link_libraries(asio OpenSSL::SSL)
 
   # Module folder
   set_target_properties(asio PROPERTIES FOLDER modules/asio)
