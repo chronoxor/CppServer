30f5ac2807f226d29bb2305299055a466f3decf4
Travis CI <travis@travis-ci.org>
2017-01-04 20:12:08 +0000

    Update generated documentation

--- search/functions_0.js
+++ search/functions_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['acceptor',['acceptor',['../class_cpp_server_1_1_asio_1_1_s_s_l_server.html#afa9b03e529931b56d26b30c924cc88b4',1,'CppServer::Asio::SSLServer::acceptor()'],['../class_cpp_server_1_1_asio_1_1_t_c_p_server.html#acfac198a2457f5e809d7eb348a105b46',1,'CppServer::Asio::TCPServer::acceptor()']]]
+  ['acceptor',['acceptor',['../class_cpp_server_1_1_asio_1_1_s_s_l_server.html#afa9b03e529931b56d26b30c924cc88b4',1,'CppServer::Asio::SSLServer::acceptor()'],['../class_cpp_server_1_1_asio_1_1_t_c_p_server.html#acfac198a2457f5e809d7eb348a105b46',1,'CppServer::Asio::TCPServer::acceptor()']]],
+  ['asio_5finitfn_5fresult_5ftype',['ASIO_INITFN_RESULT_TYPE',['../class_cpp_server_1_1_asio_1_1_service.html#a6d68a8ee6d780b65682a57fb58edb13a',1,'CppServer::Asio::Service::ASIO_INITFN_RESULT_TYPE(CompletionHandler, void()) Dispatch(ASIO_MOVE_ARG(CompletionHandler) handler)'],['../class_cpp_server_1_1_asio_1_1_service.html#a636118a30966fe37bb7631d7677d940e',1,'CppServer::Asio::Service::ASIO_INITFN_RESULT_TYPE(CompletionHandler, void()) Post(ASIO_MOVE_ARG(CompletionHandler) handler)']]]
 ];
