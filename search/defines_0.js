a57b45f2424da134f3f122f481e375e259bd96ea
Travis CI <travis@travis-ci.org>
2017-01-12 02:16:54 +0000

    Update generated documentation

--- search/defines_0.js
+++ search/defines_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['_5fwebsocketpp_5fcpp11_5fstl_5f',['_WEBSOCKETPP_CPP11_STL_',['../websocket_8h.html#a8029df4566038fda8b8286a7cc5b2a3c',1,'websocket.h']]]
+  ['_5fwebsocketpp_5fcpp11_5fstl_5f',['_WEBSOCKETPP_CPP11_STL_',['../websocket_8h.html#a8029df4566038fda8b8286a7cc5b2a3c',1,'websocket.h']]],
+  ['_5fwebsocketpp_5fcpp11_5fthread_5f',['_WEBSOCKETPP_CPP11_THREAD_',['../websocket_8h.html#a6b071d3fc2db622da3f026de9edfd99f',1,'websocket.h']]]
 ];
