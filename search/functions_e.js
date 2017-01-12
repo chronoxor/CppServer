7ab3e41a7143a849b6f2fa7b42b799d1b0d5c040
Travis CI <travis@travis-ci.org>
2017-01-12 01:57:46 +0000

    Update generated documentation

--- search/functions_e.js
+++ search/functions_e.js
@@ -1,14 +1,14 @@
 var searchData=
 [
-  ['_7eservice',['~Service',['../class_cpp_server_1_1_asio_1_1_service.html#ac68fa2770646bcad6522cc564a0c7b04',1,'CppServer::Asio::Service']]],
+  ['_7eservice',['~Service',['../class_cpp_server_1_1_asio_1_1_service.html#a265e5f4ddab91bcbcff8ac0cdf9331c6',1,'CppServer::Asio::Service']]],
   ['_7esslclient',['~SSLClient',['../class_cpp_server_1_1_asio_1_1_s_s_l_client.html#a3a454cb55dd13516fa68209ded2d5fcb',1,'CppServer::Asio::SSLClient']]],
-  ['_7esslserver',['~SSLServer',['../class_cpp_server_1_1_asio_1_1_s_s_l_server.html#ad308533805dab2894cad9ce35b28c772',1,'CppServer::Asio::SSLServer']]],
-  ['_7esslsession',['~SSLSession',['../class_cpp_server_1_1_asio_1_1_s_s_l_session.html#ab0ab6963e3882b05ea0f335980b01af0',1,'CppServer::Asio::SSLSession']]],
-  ['_7etcpclient',['~TCPClient',['../class_cpp_server_1_1_asio_1_1_t_c_p_client.html#aa03891072bfddc5e2daf7e21e71f7505',1,'CppServer::Asio::TCPClient']]],
-  ['_7etcpserver',['~TCPServer',['../class_cpp_server_1_1_asio_1_1_t_c_p_server.html#ad29a5927f780832280232a9dca494fa9',1,'CppServer::Asio::TCPServer']]],
-  ['_7etcpsession',['~TCPSession',['../class_cpp_server_1_1_asio_1_1_t_c_p_session.html#a4e2d40e708ab38b773ddab72dc15929a',1,'CppServer::Asio::TCPSession']]],
-  ['_7eudpclient',['~UDPClient',['../class_cpp_server_1_1_asio_1_1_u_d_p_client.html#abf199f57206ebdca7b631ed4945a9929',1,'CppServer::Asio::UDPClient']]],
-  ['_7eudpserver',['~UDPServer',['../class_cpp_server_1_1_asio_1_1_u_d_p_server.html#a9efb78e06fb7ca10a84386fc301ee407',1,'CppServer::Asio::UDPServer']]],
+  ['_7esslserver',['~SSLServer',['../class_cpp_server_1_1_asio_1_1_s_s_l_server.html#a2581743ccca9ee936672584bb999e98b',1,'CppServer::Asio::SSLServer']]],
+  ['_7esslsession',['~SSLSession',['../class_cpp_server_1_1_asio_1_1_s_s_l_session.html#a88e3857d4e128244d76140190e415db2',1,'CppServer::Asio::SSLSession']]],
+  ['_7etcpclient',['~TCPClient',['../class_cpp_server_1_1_asio_1_1_t_c_p_client.html#a7fa9d9d42d3e632572cca8699739d708',1,'CppServer::Asio::TCPClient']]],
+  ['_7etcpserver',['~TCPServer',['../class_cpp_server_1_1_asio_1_1_t_c_p_server.html#a20fed4176c7e34617308dfecd9936fa0',1,'CppServer::Asio::TCPServer']]],
+  ['_7etcpsession',['~TCPSession',['../class_cpp_server_1_1_asio_1_1_t_c_p_session.html#a82a76541232830cd73ee936452b973f1',1,'CppServer::Asio::TCPSession']]],
+  ['_7eudpclient',['~UDPClient',['../class_cpp_server_1_1_asio_1_1_u_d_p_client.html#a6c7a13dc2e7cf6485a0c30b174c1693a',1,'CppServer::Asio::UDPClient']]],
+  ['_7eudpserver',['~UDPServer',['../class_cpp_server_1_1_asio_1_1_u_d_p_server.html#a3b6bfce92a3cf98347bfdb0938d8c8c4',1,'CppServer::Asio::UDPServer']]],
   ['_7ewebsocketclient',['~WebSocketClient',['../class_cpp_server_1_1_asio_1_1_web_socket_client.html#ac6e6cb13d841e9f834d72a72644b40a3',1,'CppServer::Asio::WebSocketClient']]],
   ['_7ewebsocketserver',['~WebSocketServer',['../class_cpp_server_1_1_asio_1_1_web_socket_server.html#ac77c5c884e71ee9b2a80469f64479b96',1,'CppServer::Asio::WebSocketServer']]],
   ['_7ewebsocketsession',['~WebSocketSession',['../class_cpp_server_1_1_asio_1_1_web_socket_session.html#aa1da4864c6244b5d01a81a73a4605ba6',1,'CppServer::Asio::WebSocketSession']]]
