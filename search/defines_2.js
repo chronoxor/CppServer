8d57903f0b235f516d64e45c73fda085c1763736
Travis CI <travis@travis-ci.org>
2017-02-01 01:38:47 +0000

    Update generated documentation

--- search/defines_2.js
+++ search/defines_2.js
@@ -1,6 +1,4 @@
 var searchData=
 [
-  ['nanomsg_2ecpp',['nanomsg.cpp',['../nanomsg_8cpp.html',1,'']]],
-  ['nanomsg_2eh',['nanomsg.h',['../nanomsg_8h.html',1,'']]],
   ['nn_5fstatic_5flib',['NN_STATIC_LIB',['../nanomsg_8h.html#a0531c7aba215b47b6ffd197ad0f91b86',1,'nanomsg.h']]]
 ];
