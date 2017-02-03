32ad049cffdd243f498e7d8524b41a849f0d5fc5
Ivan Shynkarenka <chronoxor@gmail.com>
2017-02-04 00:39:28 +0300

    Bugfixing

--- source/server/nanomsg/survey_server.cpp
+++ source/server/nanomsg/survey_server.cpp
@@ -1,19 +1,19 @@
 /*!
-    \file surveyor_server.cpp
-    \brief Nanomsg surveyor server implementation
+    \file survey_server.cpp
+    \brief Nanomsg survey server implementation
     \author Ivan Shynkarenka
     \date 02.02.2017
     \copyright MIT License
 */
 
-#include "server/nanomsg/surveyor_server.h"
+#include "server/nanomsg/survey_server.h"
 
 #include "errors/exceptions.h"
 
 namespace CppServer {
 namespace Nanomsg {
 
-std::tuple<size_t, bool> SurveyorServer::ReceiveSurvey(Message& message)
+std::tuple<size_t, bool> SurveyServer::ReceiveSurvey(Message& message)
 {
     if (!IsStarted())
         return std::make_tuple(0, true);
@@ -29,7 +29,7 @@ std::tuple<size_t, bool> SurveyorServer::ReceiveSurvey(Message& message)
     }
 }
 
-std::tuple<size_t, bool> SurveyorServer::TryReceiveSurvey(Message& message)
+std::tuple<size_t, bool> SurveyServer::TryReceiveSurvey(Message& message)
 {
     if (!IsStarted())
         return std::make_tuple(0, true);
