e2963186f2f9c958820f1365b71bf9e16019a6b3
Ivan Shynkarenka <chronoxor@gmail.com>
2017-03-14 00:34:44 +0300

    Update

--- examples/asio_service.h
+++ examples/asio_service.h
@@ -16,16 +16,6 @@ public:
     using CppServer::Asio::Service::Service;
 
 protected:
-    void onStarted() override
-    {
-        std::cout << "Asio service started!" << std::endl;
-    }
-
-    void onStopped() override
-    {
-        std::cout << "Asio service stopped!" << std::endl;
-    }
-
     void onError(int error, const std::string& category, const std::string& message) override
     {
         std::cout << "Asio service caught an error with code " << error << " and category '" << category << "': " << message << std::endl;
