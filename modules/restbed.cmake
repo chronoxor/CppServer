e2fb8dcbcee0c56a1f4e59ed115f9d39ba3ce6b6
Ivan Shynkarenka <chronoxor@gmail.com>
2017-03-15 02:11:33 +0300

    build

--- modules/restbed.cmake
+++ modules/restbed.cmake
@@ -5,7 +5,14 @@ if(NOT TARGET restbed)
   add_library(restbed ${SOURCE_FILES})
   target_compile_definitions(restbed PRIVATE ASIO_STANDALONE ASIO_SEPARATE_COMPILATION BUILD_SSL)
   target_include_directories(restbed PRIVATE "restbed/source" PRIVATE "restbed/dependency/kashmir" PRIVATE "asio/asio/include" PRIVATE ${OPENSSL_INCLUDE_DIR})
-  #target_link_libraries(asio ${OPENSSL_LIBRARIES})
+  target_link_libraries(restbed ${OPENSSL_LIBRARIES})
+
+  # MinGW build should be manually linked with Winsock2 library
+  if(WIN32)
+    if(NOT MSVC)
+      target_link_libraries(restbed wsock32 ws2_32)
+    endif()
+  endif()
 
   # Module folder
   set_target_properties(restbed PROPERTIES FOLDER modules/restbed)
