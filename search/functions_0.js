eb8ea4e158825e0fa9c1809be2ce4ff12e53399a
AppVeyor <team@appveyor.com>
2017-03-16 23:31:30 +0000

    Update generated documentation

--- search/functions_0.js
+++ search/functions_0.js
@@ -1,8 +1,8 @@
 var searchData=
 [
-  ['accept_5ferrors',['accept_errors',['../class_cpp_server_1_1_nanomsg_1_1_socket.html#abf7302547f48f4e3cae5aba24140bdc7',1,'CppServer::Nanomsg::Socket']]],
-  ['accepted_5fconnections',['accepted_connections',['../class_cpp_server_1_1_nanomsg_1_1_socket.html#a6c0229e1004e7f5586e208e63bd2ad7b',1,'CppServer::Nanomsg::Socket']]],
+  ['accept_5ferrors',['accept_errors',['../class_cpp_server_1_1_nanomsg_1_1_socket.html#a5972eaea12944d08ea1608320e4a1c8c',1,'CppServer::Nanomsg::Socket']]],
+  ['accepted_5fconnections',['accepted_connections',['../class_cpp_server_1_1_nanomsg_1_1_socket.html#a25e1d894f2ad8dca3bc4f7d62f0c3fd2',1,'CppServer::Nanomsg::Socket']]],
   ['acceptor',['acceptor',['../class_cpp_server_1_1_asio_1_1_s_s_l_server.html#afa9b03e529931b56d26b30c924cc88b4',1,'CppServer::Asio::SSLServer::acceptor()'],['../class_cpp_server_1_1_asio_1_1_t_c_p_server.html#acfac198a2457f5e809d7eb348a105b46',1,'CppServer::Asio::TCPServer::acceptor()']]],
-  ['address',['address',['../class_cpp_server_1_1_nanomsg_1_1_socket.html#a3754a5674fb3771fcddab3b09d030f8c',1,'CppServer::Nanomsg::Socket']]],
+  ['address',['address',['../class_cpp_server_1_1_nanomsg_1_1_socket.html#ad81f75468cca448d3ea4222d46f0e8b8',1,'CppServer::Nanomsg::Socket']]],
   ['asio_5finitfn_5fresult_5ftype',['ASIO_INITFN_RESULT_TYPE',['../class_cpp_server_1_1_asio_1_1_service.html#a6d68a8ee6d780b65682a57fb58edb13a',1,'CppServer::Asio::Service::ASIO_INITFN_RESULT_TYPE(CompletionHandler, void()) Dispatch(ASIO_MOVE_ARG(CompletionHandler) handler)'],['../class_cpp_server_1_1_asio_1_1_service.html#a636118a30966fe37bb7631d7677d940e',1,'CppServer::Asio::Service::ASIO_INITFN_RESULT_TYPE(CompletionHandler, void()) Post(ASIO_MOVE_ARG(CompletionHandler) handler)']]]
 ];
