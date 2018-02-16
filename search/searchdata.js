2761c8e9f8644d28524578a4baf725abb26fd4d3
Travis CI <travis@travis-ci.org>
2018-02-16 16:53:32 +0000

    Update generated documentation

--- search/searchdata.js
+++ search/searchdata.js
@@ -1,16 +1,17 @@
 var indexSectionsWithContent =
 {
-  0: "abcdeijlmorstuv~",
-  1: "stu",
+  0: "abcdeghijlmoprstuv~",
+  1: "ahstu",
   2: "c",
-  3: "astuv",
-  4: "abcdeijlmorstu~",
+  3: "amstuv",
+  4: "abcdeghijlmorstu~",
   5: "cv",
-  6: "i",
+  6: "acdprsv",
   7: "i",
-  8: "ist",
-  9: "a",
-  10: "c"
+  8: "i",
+  9: "hist",
+  10: "a",
+  11: "c"
 };
 
 var indexSectionNames =
@@ -21,11 +22,12 @@ var indexSectionNames =
   3: "files",
   4: "functions",
   5: "variables",
-  6: "enums",
-  7: "enumvalues",
-  8: "related",
-  9: "defines",
-  10: "pages"
+  6: "typedefs",
+  7: "enums",
+  8: "enumvalues",
+  9: "related",
+  10: "defines",
+  11: "pages"
 };
 
 var indexSectionLabels =
@@ -36,10 +38,11 @@ var indexSectionLabels =
   3: "Files",
   4: "Functions",
   5: "Variables",
-  6: "Enumerations",
-  7: "Enumerator",
-  8: "Friends",
-  9: "Macros",
-  10: "Pages"
+  6: "Typedefs",
+  7: "Enumerations",
+  8: "Enumerator",
+  9: "Friends",
+  10: "Macros",
+  11: "Pages"
 };
 
