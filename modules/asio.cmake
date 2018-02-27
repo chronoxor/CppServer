d0985db18b3cf80ede21f09094111cc11ff29bcf
Ivan Shynkarenka <chronoxor@gmail.com>
2018-02-27 18:40:07 +0300

    update

--- modules/asio.cmake
+++ modules/asio.cmake
@@ -5,7 +5,7 @@ if(NOT TARGET asio)
   add_library(asio ${SOURCE_FILES})
   target_compile_definitions(asio PRIVATE ASIO_STANDALONE ASIO_SEPARATE_COMPILATION)
   target_include_directories(asio PRIVATE "asio/asio/include" PRIVATE ${OPENSSL_INCLUDE_DIR})
-  target_link_libraries(asio ${OPENSSL_LIBRARIES} ${MSWSOCK_LIBRARIES} ${WSOCK2_LIBRARIES})
+  target_link_libraries(asio ${OPENSSL_LIBRARIES})
 
   # Module folder
   set_target_properties(asio PROPERTIES FOLDER modules/asio)
