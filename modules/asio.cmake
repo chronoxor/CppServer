ff742d84bf960d9ae0fc7a84455813e3429c14be
Ivan Shynkarenka <chronoxor@gmail.com>
2018-02-28 01:50:04 +0300

    build

--- modules/asio.cmake
+++ modules/asio.cmake
@@ -2,6 +2,12 @@ if(NOT TARGET asio)
 
   # Module library
   file(GLOB SOURCE_FILES "asio/asio/src/*.cpp")
+  if(NOT MSVC)
+    set_source_files_properties(${SOURCE_FILES} PROPERTIES COMPILE_FLAGS "${PEDANTIC_COMPILE_FLAGS}")
+  else()
+    # C4996: The compiler encountered a deprecated declaration
+    set_source_files_properties(${SOURCE_FILES} PROPERTIES COMPILE_FLAGS "${PEDANTIC_COMPILE_FLAGS} /wd4996")
+  endif()
   add_library(asio ${SOURCE_FILES})
   target_compile_definitions(asio PRIVATE ASIO_STANDALONE ASIO_SEPARATE_COMPILATION)
   target_include_directories(asio PRIVATE "asio/asio/include" PRIVATE ${OPENSSL_INCLUDE_DIR})
