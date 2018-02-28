5999a8396893e12484f7139a6b76756480f52758
Travis CI <travis@travis-ci.org>
2018-02-28 01:14:48 +0000

    Update generated documentation

--- search/functions_5.js
+++ search/functions_5.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['get_5fallocator',['get_allocator',['../class_cpp_server_1_1_asio_1_1_allocate_handler.html#acf9785ea5df1e3c2f7854c2eaa39f989',1,'CppServer::Asio::AllocateHandler']]],
+  ['get_5fallocator',['get_allocator',['../class_cpp_server_1_1_asio_1_1_allocate_handler.html#a716f68697e3c2c6bcbd5d73e17c6e35d',1,'CppServer::Asio::AllocateHandler']]],
   ['getasioservice',['GetAsioService',['../class_cpp_server_1_1_asio_1_1_service.html#a5f24ffd8b61a43cfa519f2497b6b4a5c',1,'CppServer::Asio::Service']]]
 ];
