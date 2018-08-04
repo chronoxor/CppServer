e76b501921e6646387d072aad6783e375414321e
Travis CI <travis@travis-ci.org>
2018-08-04 21:17:08 +0000

    Update generated documentation

--- search/files_0.js
+++ search/files_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['asio_2ecpp',['asio.cpp',['../asio_8cpp.html',1,'']]],
-  ['asio_2eh',['asio.h',['../asio_8h.html',1,'']]]
+  ['asio_2eh',['asio.h',['../asio_8h.html',1,'']]],
+  ['asio_2einl',['asio.inl',['../asio_8inl.html',1,'']]]
 ];
