04c526fb4df3bde81071f9e1042b1ce34c48f76c
Travis CI <travis@travis-ci.org>
2019-02-04 22:12:07 +0000

    Update generated documentation

--- search/all_a.js
+++ search/all_a.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['leavemulticastgroup',['LeaveMulticastGroup',['../class_cpp_server_1_1_asio_1_1_u_d_p_client.html#a0634679e6cdb8c57391c998fa3b9a179',1,'CppServer::Asio::UDPClient']]]
+  ['leavemulticastgroupasync',['LeaveMulticastGroupAsync',['../class_cpp_server_1_1_asio_1_1_u_d_p_client.html#a5cd5d6bb0850f50148dbf968ed327fed',1,'CppServer::Asio::UDPClient']]]
 ];
