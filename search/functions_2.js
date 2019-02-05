9acbef7eb077af2f90f098fe1f657ae40d7a197f
Travis CI <travis@travis-ci.org>
2019-02-05 11:12:33 +0000

    Update generated documentation

--- search/functions_2.js
+++ search/functions_2.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['cancel',['Cancel',['../class_cpp_server_1_1_asio_1_1_timer.html#a379761043ea27f51c74bf540fe2b60dd',1,'CppServer::Asio::Timer']]],
+  ['connect',['Connect',['../class_cpp_server_1_1_asio_1_1_u_d_p_client.html#ad323993b3b207581b4f5b19f9709ca25',1,'CppServer::Asio::UDPClient']]],
   ['connectasync',['ConnectAsync',['../class_cpp_server_1_1_asio_1_1_s_s_l_client.html#a6f0734a301ce3f96a6d049cdbfd51b51',1,'CppServer::Asio::SSLClient::ConnectAsync()'],['../class_cpp_server_1_1_asio_1_1_t_c_p_client.html#abf31a320cde40db966dc0a365a2ef402',1,'CppServer::Asio::TCPClient::ConnectAsync()'],['../class_cpp_server_1_1_asio_1_1_u_d_p_client.html#afe6e7b506c7789c5b02b263745173069',1,'CppServer::Asio::UDPClient::ConnectAsync()']]],
   ['connected_5fsessions',['connected_sessions',['../class_cpp_server_1_1_asio_1_1_s_s_l_server.html#a9ac709cd20f8141ce3f50620c3f61795',1,'CppServer::Asio::SSLServer::connected_sessions()'],['../class_cpp_server_1_1_asio_1_1_t_c_p_server.html#adbcfb03aec03aaef076437be1553c66b',1,'CppServer::Asio::TCPServer::connected_sessions()']]],
   ['context',['context',['../class_cpp_server_1_1_asio_1_1_s_s_l_client.html#af27610055eaee1d4eb53e838079827f6',1,'CppServer::Asio::SSLClient::context()'],['../class_cpp_server_1_1_asio_1_1_s_s_l_server.html#aa0a6ca3d91beaba7a63102c381b93b93',1,'CppServer::Asio::SSLServer::context()']]],
--- search/functions_9.js
+++ search/functions_9.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['joinmulticastgroup',['JoinMulticastGroup',['../class_cpp_server_1_1_asio_1_1_u_d_p_client.html#a73460f8b1f6bcc2c75ddd86c56393699',1,'CppServer::Asio::UDPClient']]],
   ['joinmulticastgroupasync',['JoinMulticastGroupAsync',['../class_cpp_server_1_1_asio_1_1_u_d_p_client.html#a5d8bc414a28249a9357f6b3c808d89f8',1,'CppServer::Asio::UDPClient']]]
 ];
