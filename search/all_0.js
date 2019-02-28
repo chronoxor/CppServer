20ac85f34b8157310ded908b96b1b0ead3401987
Travis CI <travis@travis-ci.org>
2019-02-28 13:40:44 +0000

    Update generated documentation

--- search/all_0.js
+++ search/all_0.js
@@ -9,6 +9,7 @@ var searchData=
   ['asio_2eh',['asio.h',['../asio_8h.html',1,'']]],
   ['asio_2einl',['asio.inl',['../asio_8inl.html',1,'']]],
   ['asio_5finitfn_5fresult_5ftype',['ASIO_INITFN_RESULT_TYPE',['../class_cpp_server_1_1_asio_1_1_service.html#a6d68a8ee6d780b65682a57fb58edb13a',1,'CppServer::Asio::Service::ASIO_INITFN_RESULT_TYPE(CompletionHandler, void()) Dispatch(ASIO_MOVE_ARG(CompletionHandler) handler)'],['../class_cpp_server_1_1_asio_1_1_service.html#a636118a30966fe37bb7631d7677d940e',1,'CppServer::Asio::Service::ASIO_INITFN_RESULT_TYPE(CompletionHandler, void()) Post(ASIO_MOVE_ARG(CompletionHandler) handler)']]],
+  ['asio_5fno_5fwin32_5flean_5fand_5fmean',['ASIO_NO_WIN32_LEAN_AND_MEAN',['../asio_8h.html#ae32db4b16fbfb0629f334aa9b10e624a',1,'asio.h']]],
   ['asio_5fseparate_5fcompilation',['ASIO_SEPARATE_COMPILATION',['../asio_8h.html#a61c9282eacc368b1f6adb1746d7d00b6',1,'asio.h']]],
   ['asio_5fstandalone',['ASIO_STANDALONE',['../asio_8h.html#a5b90f4adb6bc09ca319c35c3448ee67a',1,'asio.h']]]
 ];
