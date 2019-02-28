797923c2236a778bcfa30fc7c6418f75febcc81f
Ivan Shynkarenka <chronoxor@gmail.com>
2019-02-28 12:39:16 +0300

    update

--- modules/asio.cmake
+++ modules/asio.cmake
@@ -2,12 +2,6 @@ if(NOT TARGET asio)
 
   # Module library
   file(GLOB SOURCE_FILES "asio/asio/src/*.cpp")
-  if(MSVC)
-    # C4267: 'var' : conversion from 'size_t' to 'type', possible loss of data
-    set_source_files_properties(${SOURCE_FILES} PROPERTIES COMPILE_FLAGS "${PEDANTIC_COMPILE_FLAGS} /wd4267")
-  else()
-    set_source_files_properties(${SOURCE_FILES} PROPERTIES COMPILE_FLAGS "${PEDANTIC_COMPILE_FLAGS}")
-  endif()
   add_library(asio ${SOURCE_FILES})
   target_compile_definitions(asio PRIVATE ASIO_STANDALONE ASIO_SEPARATE_COMPILATION)
   target_include_directories(asio PUBLIC "asio/asio/include" PUBLIC ${OPENSSL_INCLUDE_DIR})
