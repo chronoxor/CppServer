a40256953c1e9fa19436769660cc16e963888c5b
Travis CI <travis@travis-ci.org>
2019-04-22 14:31:19 +0000

    Update generated documentation

--- search/files_1.js
+++ search/files_1.js
@@ -1,10 +1,12 @@
 var searchData=
 [
   ['http_2eh',['http.h',['../http_8h.html',1,'']]],
+  ['http_5fclient_2ecpp',['http_client.cpp',['../http__client_8cpp.html',1,'']]],
   ['http_5fclient_2eh',['http_client.h',['../http__client_8h.html',1,'']]],
   ['http_5frequest_2ecpp',['http_request.cpp',['../http__request_8cpp.html',1,'']]],
   ['http_5frequest_2eh',['http_request.h',['../http__request_8h.html',1,'']]],
   ['http_5fresponse_2ecpp',['http_response.cpp',['../http__response_8cpp.html',1,'']]],
   ['http_5fresponse_2eh',['http_response.h',['../http__response_8h.html',1,'']]],
+  ['https_5fclient_2ecpp',['https_client.cpp',['../https__client_8cpp.html',1,'']]],
   ['https_5fclient_2eh',['https_client.h',['../https__client_8h.html',1,'']]]
 ];
--- search/functions_4.js
+++ search/functions_4.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['endpoint',['endpoint',['../class_cpp_server_1_1_asio_1_1_s_s_l_client.html#aa3a8368f4046d9fc802ca1087f19ebe4',1,'CppServer::Asio::SSLClient::endpoint()'],['../class_cpp_server_1_1_asio_1_1_s_s_l_server.html#a91a3a49b0709901bad25aef91999d2cd',1,'CppServer::Asio::SSLServer::endpoint()'],['../class_cpp_server_1_1_asio_1_1_t_c_p_client.html#a9e35eddda7c6b4037386e97f41100089',1,'CppServer::Asio::TCPClient::endpoint()'],['../class_cpp_server_1_1_asio_1_1_t_c_p_server.html#a8bf40125c380bcc752328ae6cd1f7a68',1,'CppServer::Asio::TCPServer::endpoint()'],['../class_cpp_server_1_1_asio_1_1_u_d_p_client.html#afbfd4d90802202b6acf8bf3ee0ec98e7',1,'CppServer::Asio::UDPClient::endpoint()'],['../class_cpp_server_1_1_asio_1_1_u_d_p_server.html#a0ade78f179eef5bedc1567f3672ea807',1,'CppServer::Asio::UDPServer::endpoint()']]],
-  ['expire_5ftime',['expire_time',['../class_cpp_server_1_1_asio_1_1_timer.html#a711de601aa6590fde072c8c98d35d291',1,'CppServer::Asio::Timer']]],
-  ['expire_5ftimespan',['expire_timespan',['../class_cpp_server_1_1_asio_1_1_timer.html#ab541ed519b868454fcc255a13699da55',1,'CppServer::Asio::Timer']]]
+  ['error',['error',['../class_cpp_server_1_1_h_t_t_p_1_1_h_t_t_p_response.html#a74d053c8eb38f28a581d251630b53de3',1,'CppServer::HTTP::HTTPResponse']]],
+  ['expire_5ftime',['expire_time',['../class_cpp_server_1_1_asio_1_1_timer.html#a50048623efaf9f5aa3eed9c888f7b8d9',1,'CppServer::Asio::Timer']]],
+  ['expire_5ftimespan',['expire_timespan',['../class_cpp_server_1_1_asio_1_1_timer.html#a62420203a4a1c8067d7cf882f7b063d2',1,'CppServer::Asio::Timer']]]
 ];
