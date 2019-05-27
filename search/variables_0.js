e6d3ff3bcaf2538e1b1f8f7b6e67516d5c880dcc
Travis CI <travis@travis-ci.org>
2019-05-27 16:08:11 +0000

    Update generated documentation

--- search/variables_0.js
+++ search/variables_0.js
@@ -1,9 +1,14 @@
 var searchData=
 [
   ['_5fhandshaked',['_handshaked',['../class_cpp_server_1_1_w_s_1_1_web_socket.html#aa3755f18010593acc558c60e8127e8cf',1,'CppServer::WS::WebSocket']]],
-  ['_5fmask',['_mask',['../class_cpp_server_1_1_w_s_1_1_web_socket.html#aedc5639b5d399f53fe3b42725042312c',1,'CppServer::WS::WebSocket']]],
   ['_5frequest',['_request',['../class_cpp_server_1_1_h_t_t_p_1_1_h_t_t_p_client.html#a50be50306b53657850e3fbfb16636259',1,'CppServer::HTTP::HTTPClient::_request()'],['../class_cpp_server_1_1_h_t_t_p_1_1_h_t_t_p_s_client.html#a234c1cb3cae1781e77dbee118ad21ef0',1,'CppServer::HTTP::HTTPSClient::_request()']]],
   ['_5fresponse',['_response',['../class_cpp_server_1_1_h_t_t_p_1_1_h_t_t_p_client.html#a3d90383626e7e7157abc55dbbd76e5cc',1,'CppServer::HTTP::HTTPClient::_response()'],['../class_cpp_server_1_1_h_t_t_p_1_1_h_t_t_p_s_client.html#a0a0c6e67bafe60a84bc25c92cd237536',1,'CppServer::HTTP::HTTPSClient::_response()']]],
+  ['_5fws_5fheader_5fsize',['_ws_header_size',['../class_cpp_server_1_1_w_s_1_1_web_socket.html#adaf02cd7859e5e1dcc556296f70e58c7',1,'CppServer::WS::WebSocket']]],
+  ['_5fws_5fpayload_5fsize',['_ws_payload_size',['../class_cpp_server_1_1_w_s_1_1_web_socket.html#a020f2f91b68aebd5e314e4aa28cc2915',1,'CppServer::WS::WebSocket']]],
+  ['_5fws_5freceive_5fbuffer',['_ws_receive_buffer',['../class_cpp_server_1_1_w_s_1_1_web_socket.html#aa142d9a8af9653b822663fd1db770501',1,'CppServer::WS::WebSocket']]],
+  ['_5fws_5freceive_5fmask',['_ws_receive_mask',['../class_cpp_server_1_1_w_s_1_1_web_socket.html#a7e787718a6cdb67218c5364cd20334c1',1,'CppServer::WS::WebSocket']]],
+  ['_5fws_5freceived',['_ws_received',['../class_cpp_server_1_1_w_s_1_1_web_socket.html#a248f40cdeb4ef83c937a88a1c1e06a00',1,'CppServer::WS::WebSocket']]],
   ['_5fws_5fsend_5fbuffer',['_ws_send_buffer',['../class_cpp_server_1_1_w_s_1_1_web_socket.html#a9d575639556725341623b0fce323f759',1,'CppServer::WS::WebSocket']]],
-  ['_5fws_5fsend_5flock',['_ws_send_lock',['../class_cpp_server_1_1_w_s_1_1_web_socket.html#a86f401fa3bf446cdee6fb02973578e7c',1,'CppServer::WS::WebSocket']]]
+  ['_5fws_5fsend_5flock',['_ws_send_lock',['../class_cpp_server_1_1_w_s_1_1_web_socket.html#a86f401fa3bf446cdee6fb02973578e7c',1,'CppServer::WS::WebSocket']]],
+  ['_5fws_5fsend_5fmask',['_ws_send_mask',['../class_cpp_server_1_1_w_s_1_1_web_socket.html#aa7f8b25c82fc478ba24faca98dc9a5d7',1,'CppServer::WS::WebSocket']]]
 ];
