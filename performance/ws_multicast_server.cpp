bd4ce4dbffcbdb786112eb3ee67bb64596d13d6a
Ivan Shynkarenka <chronoxor@gmail.com>
2019-05-28 18:25:53 +0300

    build

---
+++ performance/ws_multicast_server.cpp
@@ -0,0 +1,165 @@
+//
+// Created by Ivan Shynkarenka on 28.05.2019
+//
+
+#include "server/asio/service.h"
+#include "server/ws/ws_server.h"
+#include "system/cpu.h"
+#include "threads/thread.h"
+#include "time/timestamp.h"
+
+#include <atomic>
+#include <iostream>
+#include <thread>
+#include <vector>
+
+#include <OptionParser.h>
+
+using namespace CppCommon;
+using namespace CppServer::Asio;
+using namespace CppServer::WS;
+
+class MulticastSession : public WSSession
+{
+public:
+    using WSSession::WSSession;
+
+protected:
+    void onWSPing(const void* buffer, size_t size) override
+    {
+        SendPongAsync(buffer, size);
+    }
+
+    void onError(int error, const std::string& category, const std::string& message) override
+    {
+        std::cout << "WebSocket session caught an error with code " << error << " and category '" << category << "': " << message << std::endl;
+    }
+};
+
+class MulticastServer : public WSServer
+{
+public:
+    using WSServer::WSServer;
+
+protected:
+    std::shared_ptr<TCPSession> CreateSession(std::shared_ptr<TCPServer> server) override
+    {
+        return std::make_shared<MulticastSession>(std::dynamic_pointer_cast<WSServer>(server));
+    }
+
+protected:
+    void onError(int error, const std::string& category, const std::string& message) override
+    {
+        std::cout << "WebSocket server caught an error with code " << error << " and category '" << category << "': " << message << std::endl;
+    }
+};
+
+int main(int argc, char** argv)
+{
+    auto parser = optparse::OptionParser().version("1.0.0.0");
+
+    parser.add_option("-p", "--port").dest("port").action("store").type("int").set_default(8080).help("Server port. Default: %default");
+    parser.add_option("-t", "--threads").dest("threads").action("store").type("int").set_default(CPU::PhysicalCores()).help("Count of working threads. Default: %default");
+    parser.add_option("-m", "--messages").dest("messages").action("store").type("int").set_default(1000000).help("Rate of messages per second to send. Default: %default");
+    parser.add_option("-s", "--size").dest("size").action("store").type("int").set_default(32).help("Single message size. Default: %default");
+
+    optparse::Values options = parser.parse_args(argc, argv);
+
+    // Print help
+    if (options.get("help"))
+    {
+        parser.print_help();
+        return 0;
+    }
+
+    // Server port
+    int port = options.get("port");
+    int threads = options.get("threads");
+    int messages_rate = options.get("messages");
+    int message_size = options.get("size");
+
+    std::cout << "Server port: " << port << std::endl;
+    std::cout << "Working threads: " << threads << std::endl;
+    std::cout << "Messages rate: " << messages_rate << std::endl;
+    std::cout << "Message size: " << message_size << std::endl;
+
+    std::cout << std::endl;
+
+    // Create a new Asio service
+    auto service = std::make_shared<Service>(threads);
+
+    // Start the Asio service
+    std::cout << "Asio service starting...";
+    service->Start();
+    std::cout << "Done!" << std::endl;
+
+    // Create a new multicast server
+    auto server = std::make_shared<MulticastServer>(service, port);
+    // server->SetupNoDelay(true);
+    server->SetupReuseAddress(true);
+    server->SetupReusePort(true);
+
+    // Start the server
+    std::cout << "Server starting...";
+    server->Start();
+    std::cout << "Done!" << std::endl;
+
+    // Start the multicasting thread
+    std::atomic<bool> multicasting(true);
+    auto multicaster = std::thread([&server, &multicasting, messages_rate, message_size]()
+    {
+        // Prepare message to multicast
+        std::vector<uint8_t> message_to_send(message_size);
+
+        // Multicasting loop
+        while (multicasting)
+        {
+            auto start = UtcTimestamp();
+            for (int i = 0; i < messages_rate; ++i)
+                server->MulticastBinary(message_to_send.data(), message_to_send.size());
+            auto end = UtcTimestamp();
+
+            // Sleep for remaining time or yield
+            auto milliseconds = (end - start).milliseconds();
+            if (milliseconds < 1000)
+                Thread::Sleep(milliseconds);
+            else
+                Thread::Yield();
+        }
+    });
+
+    std::cout << "Press Enter to stop the server or '!' to restart the server..." << std::endl;
+
+    // Perform text input
+    std::string line;
+    while (getline(std::cin, line))
+    {
+        if (line.empty())
+            break;
+
+        // Restart the server
+        if (line == "!")
+        {
+            std::cout << "Server restarting...";
+            server->Restart();
+            std::cout << "Done!" << std::endl;
+            continue;
+        }
+    }
+
+    // Stop the multicasting thread
+    multicasting = false;
+    multicaster.join();
+
+    // Stop the server
+    std::cout << "Server stopping...";
+    server->Stop();
+    std::cout << "Done!" << std::endl;
+
+    // Stop the Asio service
+    std::cout << "Asio service stopping...";
+    service->Stop();
+    std::cout << "Done!" << std::endl;
+
+    return 0;
+}
---
+++ performance/wss_multicast_client.cpp
@@ -0,0 +1,179 @@
+//
+// Created by Ivan Shynkarenka on 28.05.2019
+//
+
+#include "server/asio/service.h"
+#include "server/ws/wss_client.h"
+
+#include "benchmark/reporter_console.h"
+#include "system/cpu.h"
+#include "threads/thread.h"
+#include "time/timestamp.h"
+
+#include <atomic>
+#include <iostream>
+#include <vector>
+
+#include <OptionParser.h>
+
+using namespace CppCommon;
+using namespace CppServer::Asio;
+using namespace CppServer::WS;
+
+std::atomic<uint64_t> timestamp_start(Timestamp::nano());
+std::atomic<uint64_t> timestamp_stop(Timestamp::nano());
+
+std::atomic<uint64_t> total_errors(0);
+std::atomic<uint64_t> total_bytes(0);
+std::atomic<uint64_t> total_messages(0);
+
+class MulticastClient : public WSSClient
+{
+public:
+    using WSSClient::WSSClient;
+
+protected:
+    void onWSConnecting(CppServer::HTTP::HTTPRequest& request) override
+    {
+        request.SetBegin("GET", "/");
+        request.SetHeader("Host", "localhost");
+        request.SetHeader("Origin", "https://localhost");
+        request.SetHeader("Upgrade", "websocket");
+        request.SetHeader("Connection", "Upgrade");
+        request.SetHeader("Sec-WebSocket-Key", CppCommon::Encoding::Base64Encode(id().string()));
+        request.SetHeader("Sec-WebSocket-Protocol", "chat, superchat");
+        request.SetHeader("Sec-WebSocket-Version", "13");
+    }
+
+    void onWSReceived(const void* buffer, size_t size) override
+    {
+        total_bytes += size;
+    }
+
+    void onWSPing(const void* buffer, size_t size) override
+    {
+        SendPongAsync(buffer, size);
+    }
+
+    void onError(int error, const std::string& category, const std::string& message) override
+    {
+        std::cout << "WebSocket secure client caught an error with code " << error << " and category '" << category << "': " << message << std::endl;
+        ++total_errors;
+    }
+};
+
+int main(int argc, char** argv)
+{
+    auto parser = optparse::OptionParser().version("1.0.0.0");
+
+    parser.add_option("-a", "--address").dest("address").set_default("127.0.0.1").help("Server address. Default: %default");
+    parser.add_option("-p", "--port").dest("port").action("store").type("int").set_default(8443).help("Server port. Default: %default");
+    parser.add_option("-t", "--threads").dest("threads").action("store").type("int").set_default(CPU::PhysicalCores()).help("Count of working threads. Default: %default");
+    parser.add_option("-c", "--clients").dest("clients").action("store").type("int").set_default(100).help("Count of working clients. Default: %default");
+    parser.add_option("-s", "--size").dest("size").action("store").type("int").set_default(32).help("Single message size. Default: %default");
+    parser.add_option("-z", "--seconds").dest("seconds").action("store").type("int").set_default(10).help("Count of seconds to benchmarking. Default: %default");
+
+    optparse::Values options = parser.parse_args(argc, argv);
+
+    // Print help
+    if (options.get("help"))
+    {
+        parser.print_help();
+        return 0;
+    }
+
+    // Client parameters
+    std::string address(options.get("address"));
+    int port = options.get("port");
+    int threads_count = options.get("threads");
+    int clients_count = options.get("clients");
+    int message_size = options.get("size");
+    int seconds_count = options.get("seconds");
+
+    std::cout << "Server address: " << address << std::endl;
+    std::cout << "Server port: " << port << std::endl;
+    std::cout << "Working threads: " << threads_count << std::endl;
+    std::cout << "Working clients: " << clients_count << std::endl;
+    std::cout << "Message size: " << message_size << std::endl;
+    std::cout << "Seconds to benchmarking: " << seconds_count << std::endl;
+
+    std::cout << std::endl;
+
+    // Create a new Asio service
+    auto service = std::make_shared<Service>(threads_count);
+
+    // Start the Asio service
+    std::cout << "Asio service starting...";
+    service->Start();
+    std::cout << "Done!" << std::endl;
+
+    // Create and prepare a new SSL client context
+    auto context = std::make_shared<CppServer::Asio::SSLContext>(asio::ssl::context::tlsv12);
+    context->set_default_verify_paths();
+    context->set_root_certs();
+    context->set_verify_mode(asio::ssl::verify_peer | asio::ssl::verify_fail_if_no_peer_cert);
+    context->load_verify_file("../tools/certificates/ca.pem");
+
+    // Create multicast clients
+    std::vector<std::shared_ptr<MulticastClient>> clients;
+    for (int i = 0; i < clients_count; ++i)
+    {
+        auto client = std::make_shared<MulticastClient>(service, context, address, port);
+        // client->SetupNoDelay(true);
+        clients.emplace_back(client);
+    }
+
+    timestamp_start = Timestamp::nano();
+
+    // Connect clients
+    std::cout << "Clients connecting...";
+    for (auto& client : clients)
+        client->ConnectAsync();
+    std::cout << "Done!" << std::endl;
+    for (const auto& client : clients)
+        while (!client->IsConnected())
+            Thread::Yield();
+    std::cout << "All clients connected!" << std::endl;
+
+    // Wait for benchmarking
+    std::cout << "Benchmarking...";
+    Thread::Sleep(seconds_count * 1000);
+    std::cout << "Done!" << std::endl;
+
+    // Disconnect clients
+    std::cout << "Clients disconnecting...";
+    for (auto& client : clients)
+        client->DisconnectAsync(1000);
+    std::cout << "Done!" << std::endl;
+    for (const auto& client : clients)
+        while (client->IsConnected())
+            Thread::Yield();
+    std::cout << "All clients disconnected!" << std::endl;
+
+    timestamp_stop = Timestamp::nano();
+
+    // Stop the Asio service
+    std::cout << "Asio service stopping...";
+    service->Stop();
+    std::cout << "Done!" << std::endl;
+
+    std::cout << std::endl;
+
+    std::cout << "Errors: " << total_errors << std::endl;
+
+    std::cout << std::endl;
+
+    total_messages = total_bytes / message_size;
+
+    std::cout << "Total time: " << CppBenchmark::ReporterConsole::GenerateTimePeriod(timestamp_stop - timestamp_start) << std::endl;
+    std::cout << "Total data: " << CppBenchmark::ReporterConsole::GenerateDataSize(total_bytes) << std::endl;
+    std::cout << "Total messages: " << total_messages << std::endl;
+    std::cout << "Data throughput: " << CppBenchmark::ReporterConsole::GenerateDataSize(total_bytes * 1000000000 / (timestamp_stop - timestamp_start)) << "/s" << std::endl;
+    if (total_messages > 0)
+    {
+        std::cout << "Message latency: " << CppBenchmark::ReporterConsole::GenerateTimePeriod((timestamp_stop - timestamp_start) / total_messages) << std::endl;
+        std::cout << "Message throughput: " << total_messages * 1000000000 / (timestamp_stop - timestamp_start) << " msg/s" << std::endl;
+    }
+
+    return 0;
+}
