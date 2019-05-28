a294d1d08d25b24ad60ed8123871da4605bbe39e
Travis CI <travis@travis-ci.org>
2019-05-28 23:39:29 +0000

    Update generated documentation

--- search/files_7.js
+++ search/files_7.js
@@ -4,11 +4,13 @@ var searchData=
   ['ws_2eh',['ws.h',['../ws_8h.html',1,'']]],
   ['ws_5fclient_2ecpp',['ws_client.cpp',['../ws__client_8cpp.html',1,'']]],
   ['ws_5fclient_2eh',['ws_client.h',['../ws__client_8h.html',1,'']]],
+  ['ws_5fserver_2ecpp',['ws_server.cpp',['../ws__server_8cpp.html',1,'']]],
   ['ws_5fserver_2eh',['ws_server.h',['../ws__server_8h.html',1,'']]],
   ['ws_5fsession_2ecpp',['ws_session.cpp',['../ws__session_8cpp.html',1,'']]],
   ['ws_5fsession_2eh',['ws_session.h',['../ws__session_8h.html',1,'']]],
   ['wss_5fclient_2ecpp',['wss_client.cpp',['../wss__client_8cpp.html',1,'']]],
   ['wss_5fclient_2eh',['wss_client.h',['../wss__client_8h.html',1,'']]],
+  ['wss_5fserver_2ecpp',['wss_server.cpp',['../wss__server_8cpp.html',1,'']]],
   ['wss_5fserver_2eh',['wss_server.h',['../wss__server_8h.html',1,'']]],
   ['wss_5fsession_2ecpp',['wss_session.cpp',['../wss__session_8cpp.html',1,'']]],
   ['wss_5fsession_2eh',['wss_session.h',['../wss__session_8h.html',1,'']]]
