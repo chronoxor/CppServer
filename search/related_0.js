8413acf25945ef315dcb0376aaab02a387dab8c5
Travis CI <travis@travis-ci.org>
2020-02-27 23:38:32 +0000

    Update generated documentation

--- search/related_0.js
+++ search/related_0.js
@@ -1,8 +1,8 @@
 var searchData=
 [
-  ['handlerallocator',['HandlerAllocator',['../class_cpp_server_1_1_asio_1_1_handler_allocator.html#acee75256efb232f7a5f2c43477331435',1,'CppServer::Asio::HandlerAllocator']]],
-  ['httpclient',['HTTPClient',['../class_cpp_server_1_1_h_t_t_p_1_1_h_t_t_p_response.html#af6c1c2fe7f9b1a64078d25a9fba05eb6',1,'CppServer::HTTP::HTTPResponse']]],
-  ['httpsclient',['HTTPSClient',['../class_cpp_server_1_1_h_t_t_p_1_1_h_t_t_p_response.html#a01c408137817e946e8c49bfd83119d8d',1,'CppServer::HTTP::HTTPResponse']]],
-  ['httpsession',['HTTPSession',['../class_cpp_server_1_1_h_t_t_p_1_1_h_t_t_p_request.html#ae7d788b643cb6462359521d4e3820ecc',1,'CppServer::HTTP::HTTPRequest']]],
-  ['httpssession',['HTTPSSession',['../class_cpp_server_1_1_h_t_t_p_1_1_h_t_t_p_request.html#a100373703aa72c9e6ec297a92358be3f',1,'CppServer::HTTP::HTTPRequest']]]
+  ['handlerallocator_752',['HandlerAllocator',['../class_cpp_server_1_1_asio_1_1_handler_allocator.html#acee75256efb232f7a5f2c43477331435',1,'CppServer::Asio::HandlerAllocator']]],
+  ['httpclient_753',['HTTPClient',['../class_cpp_server_1_1_h_t_t_p_1_1_h_t_t_p_response.html#af6c1c2fe7f9b1a64078d25a9fba05eb6',1,'CppServer::HTTP::HTTPResponse']]],
+  ['httpsclient_754',['HTTPSClient',['../class_cpp_server_1_1_h_t_t_p_1_1_h_t_t_p_response.html#a01c408137817e946e8c49bfd83119d8d',1,'CppServer::HTTP::HTTPResponse']]],
+  ['httpsession_755',['HTTPSession',['../class_cpp_server_1_1_h_t_t_p_1_1_h_t_t_p_request.html#ae7d788b643cb6462359521d4e3820ecc',1,'CppServer::HTTP::HTTPRequest']]],
+  ['httpssession_756',['HTTPSSession',['../class_cpp_server_1_1_h_t_t_p_1_1_h_t_t_p_request.html#a100373703aa72c9e6ec297a92358be3f',1,'CppServer::HTTP::HTTPRequest']]]
 ];
