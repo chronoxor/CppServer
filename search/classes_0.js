3cc0b2ba85f05a764c4dd4452b533fce1a4f1247
Travis CI <travis@travis-ci.org>
2020-02-28 00:04:59 +0000

    Update generated documentation

--- search/classes_0.js
+++ search/classes_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['allocatehandler_363',['AllocateHandler',['../class_cpp_server_1_1_asio_1_1_allocate_handler.html',1,'CppServer::Asio']]]
+  ['allocatehandler',['AllocateHandler',['../class_cpp_server_1_1_asio_1_1_allocate_handler.html',1,'CppServer::Asio']]]
 ];
--- search/classes_4.js
+++ search/classes_4.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['udpclient_388',['UDPClient',['../class_cpp_server_1_1_asio_1_1_u_d_p_client.html',1,'CppServer::Asio']]],
-  ['udpresolver_389',['UDPResolver',['../class_cpp_server_1_1_asio_1_1_u_d_p_resolver.html',1,'CppServer::Asio']]],
-  ['udpserver_390',['UDPServer',['../class_cpp_server_1_1_asio_1_1_u_d_p_server.html',1,'CppServer::Asio']]]
+  ['udpclient',['UDPClient',['../class_cpp_server_1_1_asio_1_1_u_d_p_client.html',1,'CppServer::Asio']]],
+  ['udpresolver',['UDPResolver',['../class_cpp_server_1_1_asio_1_1_u_d_p_resolver.html',1,'CppServer::Asio']]],
+  ['udpserver',['UDPServer',['../class_cpp_server_1_1_asio_1_1_u_d_p_server.html',1,'CppServer::Asio']]]
 ];
