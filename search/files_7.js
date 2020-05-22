414ce65a75d2a407802cc6da46e2373a6cc488c9
Travis CI <travis@travis-ci.org>
2020-05-22 21:49:42 +0000

    Update generated documentation

--- search/files_7.js
+++ search/files_7.js
@@ -1,17 +1,17 @@
 var searchData=
 [
-  ['ws_2ecpp_453',['ws.cpp',['../ws_8cpp.html',1,'']]],
-  ['ws_2eh_454',['ws.h',['../ws_8h.html',1,'']]],
-  ['ws_5fclient_2ecpp_455',['ws_client.cpp',['../ws__client_8cpp.html',1,'']]],
-  ['ws_5fclient_2eh_456',['ws_client.h',['../ws__client_8h.html',1,'']]],
-  ['ws_5fserver_2ecpp_457',['ws_server.cpp',['../ws__server_8cpp.html',1,'']]],
-  ['ws_5fserver_2eh_458',['ws_server.h',['../ws__server_8h.html',1,'']]],
-  ['ws_5fsession_2ecpp_459',['ws_session.cpp',['../ws__session_8cpp.html',1,'']]],
-  ['ws_5fsession_2eh_460',['ws_session.h',['../ws__session_8h.html',1,'']]],
-  ['wss_5fclient_2ecpp_461',['wss_client.cpp',['../wss__client_8cpp.html',1,'']]],
-  ['wss_5fclient_2eh_462',['wss_client.h',['../wss__client_8h.html',1,'']]],
-  ['wss_5fserver_2ecpp_463',['wss_server.cpp',['../wss__server_8cpp.html',1,'']]],
-  ['wss_5fserver_2eh_464',['wss_server.h',['../wss__server_8h.html',1,'']]],
-  ['wss_5fsession_2ecpp_465',['wss_session.cpp',['../wss__session_8cpp.html',1,'']]],
-  ['wss_5fsession_2eh_466',['wss_session.h',['../wss__session_8h.html',1,'']]]
+  ['ws_2ecpp',['ws.cpp',['../ws_8cpp.html',1,'']]],
+  ['ws_2eh',['ws.h',['../ws_8h.html',1,'']]],
+  ['ws_5fclient_2ecpp',['ws_client.cpp',['../ws__client_8cpp.html',1,'']]],
+  ['ws_5fclient_2eh',['ws_client.h',['../ws__client_8h.html',1,'']]],
+  ['ws_5fserver_2ecpp',['ws_server.cpp',['../ws__server_8cpp.html',1,'']]],
+  ['ws_5fserver_2eh',['ws_server.h',['../ws__server_8h.html',1,'']]],
+  ['ws_5fsession_2ecpp',['ws_session.cpp',['../ws__session_8cpp.html',1,'']]],
+  ['ws_5fsession_2eh',['ws_session.h',['../ws__session_8h.html',1,'']]],
+  ['wss_5fclient_2ecpp',['wss_client.cpp',['../wss__client_8cpp.html',1,'']]],
+  ['wss_5fclient_2eh',['wss_client.h',['../wss__client_8h.html',1,'']]],
+  ['wss_5fserver_2ecpp',['wss_server.cpp',['../wss__server_8cpp.html',1,'']]],
+  ['wss_5fserver_2eh',['wss_server.h',['../wss__server_8h.html',1,'']]],
+  ['wss_5fsession_2ecpp',['wss_session.cpp',['../wss__session_8cpp.html',1,'']]],
+  ['wss_5fsession_2eh',['wss_session.h',['../wss__session_8h.html',1,'']]]
 ];
