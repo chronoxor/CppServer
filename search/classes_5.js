4bf1a449c64a3d59661b624b82ff013574ec069d
Travis CI <travis@travis-ci.org>
2020-06-01 20:34:36 +0000

    Update generated documentation

--- search/classes_5.js
+++ search/classes_5.js
@@ -1,10 +1,10 @@
 var searchData=
 [
-  ['websocket',['WebSocket',['../class_cpp_server_1_1_w_s_1_1_web_socket.html',1,'CppServer::WS']]],
-  ['wsclient',['WSClient',['../class_cpp_server_1_1_w_s_1_1_w_s_client.html',1,'CppServer::WS']]],
-  ['wssclient',['WSSClient',['../class_cpp_server_1_1_w_s_1_1_w_s_s_client.html',1,'CppServer::WS']]],
-  ['wsserver',['WSServer',['../class_cpp_server_1_1_w_s_1_1_w_s_server.html',1,'CppServer::WS']]],
-  ['wssession',['WSSession',['../class_cpp_server_1_1_w_s_1_1_w_s_session.html',1,'CppServer::WS']]],
-  ['wssserver',['WSSServer',['../class_cpp_server_1_1_w_s_1_1_w_s_s_server.html',1,'CppServer::WS']]],
-  ['wsssession',['WSSSession',['../class_cpp_server_1_1_w_s_1_1_w_s_s_session.html',1,'CppServer::WS']]]
+  ['websocket_391',['WebSocket',['../class_cpp_server_1_1_w_s_1_1_web_socket.html',1,'CppServer::WS']]],
+  ['wsclient_392',['WSClient',['../class_cpp_server_1_1_w_s_1_1_w_s_client.html',1,'CppServer::WS']]],
+  ['wssclient_393',['WSSClient',['../class_cpp_server_1_1_w_s_1_1_w_s_s_client.html',1,'CppServer::WS']]],
+  ['wsserver_394',['WSServer',['../class_cpp_server_1_1_w_s_1_1_w_s_server.html',1,'CppServer::WS']]],
+  ['wssession_395',['WSSession',['../class_cpp_server_1_1_w_s_1_1_w_s_session.html',1,'CppServer::WS']]],
+  ['wssserver_396',['WSSServer',['../class_cpp_server_1_1_w_s_1_1_w_s_s_server.html',1,'CppServer::WS']]],
+  ['wsssession_397',['WSSSession',['../class_cpp_server_1_1_w_s_1_1_w_s_s_session.html',1,'CppServer::WS']]]
 ];
