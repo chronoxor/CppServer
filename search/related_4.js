44ff75c655e95bd2032567fd9ad5388e909e9360
Travis CI <travis@travis-ci.org>
2020-07-12 21:27:09 +0000

    Update generated documentation

--- search/related_4.js
+++ search/related_4.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['tcpserver',['TCPServer',['../class_cpp_server_1_1_asio_1_1_t_c_p_session.html#ae4cfdb1814d91a8d28dadb49adda68f0',1,'CppServer::Asio::TCPSession']]],
-  ['tcpsession',['TCPSession',['../class_cpp_server_1_1_asio_1_1_t_c_p_server.html#aa912e3cd6b0bf526562fb81d91e6b63b',1,'CppServer::Asio::TCPServer']]]
+  ['tcpserver_762',['TCPServer',['../class_cpp_server_1_1_asio_1_1_t_c_p_session.html#ae4cfdb1814d91a8d28dadb49adda68f0',1,'CppServer::Asio::TCPSession']]],
+  ['tcpsession_763',['TCPSession',['../class_cpp_server_1_1_asio_1_1_t_c_p_server.html#aa912e3cd6b0bf526562fb81d91e6b63b',1,'CppServer::Asio::TCPServer']]]
 ];
--- search/typedefs_2.js
+++ search/typedefs_2.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['difference_5ftype',['difference_type',['../class_cpp_server_1_1_asio_1_1_handler_allocator.html#aaf9a544c42d9226deb9278a38ef131a3',1,'CppServer::Asio::HandlerAllocator']]]
+  ['difference_5ftype_743',['difference_type',['../class_cpp_server_1_1_asio_1_1_handler_allocator.html#aaf9a544c42d9226deb9278a38ef131a3',1,'CppServer::Asio::HandlerAllocator']]]
 ];
