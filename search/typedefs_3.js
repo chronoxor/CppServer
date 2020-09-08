305e988c112eef26b460838faecddd9fbe14423c
Travis CI <travis@travis-ci.org>
2020-09-08 21:19:35 +0000

    Update generated documentation

--- search/typedefs_3.js
+++ search/typedefs_3.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['pointer_744',['pointer',['../class_cpp_server_1_1_asio_1_1_handler_allocator.html#afc0fc605d880945d55991cd4f1838746',1,'CppServer::Asio::HandlerAllocator']]]
+  ['reference_745',['reference',['../class_cpp_server_1_1_asio_1_1_handler_allocator.html#a53603c2e0b1480bd010d870f676e6568',1,'CppServer::Asio::HandlerAllocator']]],
+  ['result_5ftype_746',['result_type',['../structstd_1_1hash_3_01_cpp_server_1_1_h_t_t_p_1_1_h_t_t_p_request_01_4.html#ac5e24f4bdb55beefa1a0e7f4d7f706f2',1,'std::hash&lt; CppServer::HTTP::HTTPRequest &gt;::result_type()'],['../structstd_1_1hash_3_01_cpp_server_1_1_h_t_t_p_1_1_h_t_t_p_response_01_4.html#ac33746515be0b66bc031854ddec637a2',1,'std::hash&lt; CppServer::HTTP::HTTPResponse &gt;::result_type()']]]
 ];
