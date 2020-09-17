99c9d6b9c0a7358cd6fa3df16c0b3ac140ea772b
Travis CI <travis@travis-ci.org>
2020-09-17 02:34:41 +0000

    Update generated documentation

--- search/variables_2.js
+++ search/variables_2.js
@@ -1,4 +1,9 @@
 var searchData=
 [
-  ['version_734',['version',['../namespace_cpp_server.html#a972082695170ea74467c904dfb4e5038',1,'CppServer']]]
+  ['ws_5fbinary_733',['WS_BINARY',['../class_cpp_server_1_1_w_s_1_1_web_socket.html#a42d49f427563a2fc1ce56e3b5ea2eff0',1,'CppServer::WS::WebSocket']]],
+  ['ws_5fclose_734',['WS_CLOSE',['../class_cpp_server_1_1_w_s_1_1_web_socket.html#a0239a78a9a0aca4103657c097b048489',1,'CppServer::WS::WebSocket']]],
+  ['ws_5ffin_735',['WS_FIN',['../class_cpp_server_1_1_w_s_1_1_web_socket.html#a6b0bdc56aebba8fa73669a38185b4300',1,'CppServer::WS::WebSocket']]],
+  ['ws_5fping_736',['WS_PING',['../class_cpp_server_1_1_w_s_1_1_web_socket.html#a51b19be56e3ff68ca351e62f3dd9c88d',1,'CppServer::WS::WebSocket']]],
+  ['ws_5fpong_737',['WS_PONG',['../class_cpp_server_1_1_w_s_1_1_web_socket.html#a92d9fe3692ebe69d9f9e7e7ae6e46c65',1,'CppServer::WS::WebSocket']]],
+  ['ws_5ftext_738',['WS_TEXT',['../class_cpp_server_1_1_w_s_1_1_web_socket.html#a0b711849819d16f70a590b86155de360',1,'CppServer::WS::WebSocket']]]
 ];
