2e2151a1a7bdcb4de90bc746c94e3b04b319f3e1
chronoxor <chronoxor@users.noreply.github.com>
2026-05-27 02:42:32 +0000

    Update generated documentation

--- search/functions_6.js
+++ search/functions_6.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['get_5fallocator_0',['get_allocator',['../class_cpp_server_1_1_asio_1_1_allocate_handler.html#a716f68697e3c2c6bcbd5d73e17c6e35d',1,'CppServer::Asio::AllocateHandler']]],
-  ['getasiocontext_1',['GetAsioContext',['../class_cpp_server_1_1_asio_1_1_service.html#a42b8d0553095613a244d845a75b7da49',1,'CppServer::Asio::Service']]]
+  ['getasioservice_1',['GetAsioService',['../class_cpp_server_1_1_asio_1_1_service.html#ac074c5fa48bae78c86ff1bfdd77a76f7',1,'CppServer::Asio::Service']]]
 ];
