6e575dc6e7be4cd694ddb0a0f643db1f094caa8b
chronoxor <chronoxor@users.noreply.github.com>
2026-05-27 00:24:29 +0000

    Update generated documentation

--- search/classes_1.js
+++ search/classes_1.js
@@ -1,15 +1,16 @@
 var searchData=
 [
   ['handlerallocator_0',['HandlerAllocator',['../class_cpp_server_1_1_asio_1_1_handler_allocator.html',1,'CppServer::Asio']]],
-  ['handlerstorage_1',['HandlerStorage',['../class_cpp_server_1_1_asio_1_1_handler_storage.html',1,'CppServer::Asio']]],
-  ['httpclient_2',['HTTPClient',['../class_cpp_server_1_1_h_t_t_p_1_1_h_t_t_p_client.html',1,'CppServer::HTTP']]],
-  ['httpclientex_3',['HTTPClientEx',['../class_cpp_server_1_1_h_t_t_p_1_1_h_t_t_p_client_ex.html',1,'CppServer::HTTP']]],
-  ['httprequest_4',['HTTPRequest',['../class_cpp_server_1_1_h_t_t_p_1_1_h_t_t_p_request.html',1,'CppServer::HTTP']]],
-  ['httpresponse_5',['HTTPResponse',['../class_cpp_server_1_1_h_t_t_p_1_1_h_t_t_p_response.html',1,'CppServer::HTTP']]],
-  ['httpsclient_6',['HTTPSClient',['../class_cpp_server_1_1_h_t_t_p_1_1_h_t_t_p_s_client.html',1,'CppServer::HTTP']]],
-  ['httpsclientex_7',['HTTPSClientEx',['../class_cpp_server_1_1_h_t_t_p_1_1_h_t_t_p_s_client_ex.html',1,'CppServer::HTTP']]],
-  ['httpserver_8',['HTTPServer',['../class_cpp_server_1_1_h_t_t_p_1_1_h_t_t_p_server.html',1,'CppServer::HTTP']]],
-  ['httpsession_9',['HTTPSession',['../class_cpp_server_1_1_h_t_t_p_1_1_h_t_t_p_session.html',1,'CppServer::HTTP']]],
-  ['httpsserver_10',['HTTPSServer',['../class_cpp_server_1_1_h_t_t_p_1_1_h_t_t_p_s_server.html',1,'CppServer::HTTP']]],
-  ['httpssession_11',['HTTPSSession',['../class_cpp_server_1_1_h_t_t_p_1_1_h_t_t_p_s_session.html',1,'CppServer::HTTP']]]
+  ['handlerallocator_3c_20thandler_20_3e_1',['HandlerAllocator&lt; THandler &gt;',['../class_cpp_server_1_1_asio_1_1_handler_allocator.html',1,'CppServer::Asio']]],
+  ['handlerstorage_2',['HandlerStorage',['../class_cpp_server_1_1_asio_1_1_handler_storage.html',1,'CppServer::Asio']]],
+  ['httpclient_3',['HTTPClient',['../class_cpp_server_1_1_h_t_t_p_1_1_h_t_t_p_client.html',1,'CppServer::HTTP']]],
+  ['httpclientex_4',['HTTPClientEx',['../class_cpp_server_1_1_h_t_t_p_1_1_h_t_t_p_client_ex.html',1,'CppServer::HTTP']]],
+  ['httprequest_5',['HTTPRequest',['../class_cpp_server_1_1_h_t_t_p_1_1_h_t_t_p_request.html',1,'CppServer::HTTP']]],
+  ['httpresponse_6',['HTTPResponse',['../class_cpp_server_1_1_h_t_t_p_1_1_h_t_t_p_response.html',1,'CppServer::HTTP']]],
+  ['httpsclient_7',['HTTPSClient',['../class_cpp_server_1_1_h_t_t_p_1_1_h_t_t_p_s_client.html',1,'CppServer::HTTP']]],
+  ['httpsclientex_8',['HTTPSClientEx',['../class_cpp_server_1_1_h_t_t_p_1_1_h_t_t_p_s_client_ex.html',1,'CppServer::HTTP']]],
+  ['httpserver_9',['HTTPServer',['../class_cpp_server_1_1_h_t_t_p_1_1_h_t_t_p_server.html',1,'CppServer::HTTP']]],
+  ['httpsession_10',['HTTPSession',['../class_cpp_server_1_1_h_t_t_p_1_1_h_t_t_p_session.html',1,'CppServer::HTTP']]],
+  ['httpsserver_11',['HTTPSServer',['../class_cpp_server_1_1_h_t_t_p_1_1_h_t_t_p_s_server.html',1,'CppServer::HTTP']]],
+  ['httpssession_12',['HTTPSSession',['../class_cpp_server_1_1_h_t_t_p_1_1_h_t_t_p_s_session.html',1,'CppServer::HTTP']]]
 ];
--- search/related_1.js
+++ search/related_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['operator_3c_3c_0',['operator&lt;&lt;',['../class_cpp_server_1_1_h_t_t_p_1_1_h_t_t_p_request.html#ae0b526fdd145c87e59046dbfc5805b37',1,'CppServer::HTTP::HTTPRequest::operator&lt;&lt;'],['../class_cpp_server_1_1_h_t_t_p_1_1_h_t_t_p_response.html#af1c27343e3f72355a4a7c2b8d7331670',1,'CppServer::HTTP::HTTPResponse::operator&lt;&lt;']]]
+  ['operator_3c_3c_0',['operator&lt;&lt;',['../class_cpp_server_1_1_h_t_t_p_1_1_h_t_t_p_request.html#ae0b526fdd145c87e59046dbfc5805b37',1,'CppServer::HTTP::HTTPRequest::operator&lt;&lt;()'],['../class_cpp_server_1_1_h_t_t_p_1_1_h_t_t_p_response.html#af1c27343e3f72355a4a7c2b8d7331670',1,'CppServer::HTTP::HTTPResponse::operator&lt;&lt;()']]]
 ];
